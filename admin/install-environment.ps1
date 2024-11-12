2bf8cfa98db64c1f6b24c23c2d36632c2fc0cb41
Nicolaj Kirchhof <nico@docoyo.com>
2024-11-12 12:13:43 +0100

    Updated backtesting script, added dependencies

---
+++ admin/install-environment.ps1
@@ -0,0 +1,3 @@
+conda create -n py311 python=3.11 ipython matplotlib qt pyqt pandas yfinance numpy scipy spyder scikit-learn mplfinancce vectorbt 
+conda activate py311
+pip install backtesting pysystemtrade 
--- finance/testing-strategies.py
+++ finance/testing-strategies.py
@@ -1,5 +1,7 @@
+#%%
 import yfinance as yf
 import pandas as pd
+import mplfinance as mpf
 
 import matplotlib.pyplot as plt
 %matplotlib qt
@@ -14,11 +16,47 @@ tickers.tickers['GOOG'].actions
 
 tickers.tickers['MSFT'].info
 #%%
-data = yf.download("FJTSY", start="2023-11-08", interval="1h")
+import finplot as fplt
+# import yfinance
+
+# fdf = yfinance.download('AAPL')
+fplt.candlestick_ochl(df[['Open', 'Close', 'High', 'Low']])
+fplt.show()
 #%%
-data.plot()
+# data = yf.download("FJTSY", start="2023-11-08", interval="1h")
+
+fjtsy = yf.Ticker('fjtsy')
+df = fjtsy.history(start="2024-01-01", interval="1h")
+# df = fjtsy.history(start="2024-10-08", interval="1h")
 
 #%%
-df = data
+df['CloseMa7'] = df['Close'].rolling(window=7).mean()
+df['HighMa7'] = df['High'].rolling(window=7).mean()
+df['LowMa7'] = df['Low'].rolling(window=7).mean()
+df['CloseMa21'] = df['Close'].rolling(window=21).mean()
+df['HighMa21'] = df['High'].rolling(window=21).mean()
+df['LowMa21'] = df['Low'].rolling(window=21).mean()
 
-df['Close MA'] = df['Close'].rolling(window=7).mean()
+#%% 
+  
+
+df['OrderClosePre']= (df['CloseMa7'].shift(1) > df['CloseMa21'].shift(1)).astype(int)
+df['OrderClose'] = (df['CloseMa7'] > df['CloseMa21']).astype(int) - df['OrderClosePre']
+
+buys = df['OrderClose'].where(df['OrderClose'] > 0).multiply(df['Open'])
+sels = df['OrderClose'].where(df['OrderClose'] < 0).multiply(df['Open']).multiply(-1)
+
+#%%
+# mpf.plot(df, returnfig=True, volume=True, tight_layout=True)
+aplt = [mpf.make_addplot(df[['CloseMa7', 'CloseMa21']]), 
+        mpf.make_addplot(buys, type='scatter', markersize=100, marker='^'),
+        mpf.make_addplot(sels, type='scatter', markersize=100, marker='v')]
+#
+
+mpf.plot(df, volume=True, tight_layout=True, addplot=aplt)
+
+#%%
+plt.cla()
+fig, ax = mpf.plot(df, returnfig=True, volume=True, type='candle', mav=(7,21), tight_layout=True)
+
+df.plot(ax[0], y=['CloseMa', 'HighMa', 'LowMa'])
