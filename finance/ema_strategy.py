b6580ae39b7e6b0c608fda702dabfb7dde3003af
Nicolaj Kirchhof <nico@docoyo.com>
2024-12-02 02:19:45 +0100

    Refactor finance scripts and update misc.xml configuration.
    Streamlined imports and logging in `ib_async_test.py`, updated connection ID, and modified data request methodology. Improved clarity in `EmaStrategy` by updating moving average initialization and simplifying logging statements. Additionally, configured `.idea/misc.xml` with a new component for Python version compatibility inspection.

--- finance/ema_strategy.py
+++ finance/ema_strategy.py
@@ -3,8 +3,9 @@ import backtrader as bt
 # Create a Stratey
 class EmaStrategy(bt.Strategy):
   def __init__(self):
-    self.sma_5 = bt.ind.ExponentialMovingAverage(5)
-    self.sma_9 = bt.ind.ExponentialMovingAverage(9)
+    print('init 5/9 EMA')
+    self.sma_5 = bt.ind.EMA(period=5)
+    self.sma_9 = bt.ind.EMA(period=9)
     self.close = self.datas[0].close
 
   def log(self, txt, ts=None):
@@ -13,14 +14,9 @@ class EmaStrategy(bt.Strategy):
     print(f'{ts}, {txt}')
 
   def next(self):
-  # Current close
-    # self.log(self)
-    self.log(f'Open: {self.datas[0].open :. 2f}, \
-    High: {self.datas[0].high :. 2f}, \
-    Low: {self.datas[0].low[0] :. 2f}, \
-    Close: {self.datas[0].close[0] :. 2f}, \
-    Volume: {self.datas[0].volume [0] :. 2f}')
-    # self. log(f'Close:{self.close[0] :. 2f}' )
+    # Current close
+    self.log(f'Open: {self.datas[0].open[-1]:.2f}, \
+    Close: {self.close[0]:.2f}')
     # if self.close[0] < self.close[-1]:
     #   # current close less than previous close, think about buying
     #   if self.close[-1] < self.close[-2]:
--- finance/ib_async_test.py
+++ finance/ib_async_test.py
@@ -1,12 +1,19 @@
 #%%
-from __future__ import (absolute_import, division, print_function, unicode_literals)
-from ib_async import *
+# from __future__ import (absolute_import, division, print_function, unicode_literals)
+from ib_async import ib, util, IB, Forex, Stock
 # util.startLoop()  # uncomment this line when in a notebook
 import backtrader as bt
-import matplotlib.pyplot as plt
 
-from ema_strategy import EmaStrategy
-%matplotlib qt
+
+from finance.ema_strategy import EmaStrategy
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.use('TkAgg')
+mpl.use('QtAgg')
+%load_ext autoreload
+%autoreload 2
 
 #%%
 util.startLoop()
@@ -15,26 +22,41 @@ tws_real_port = 7497
 tws_paper_port = 7497
 api_real_port = 4002 
 api_paper_port = 4002 
-ib.connect('127.0.0.1', api_paper_port, clientId=1, readonly=True)
-
+ib.connect('127.0.0.1', api_paper_port, clientId=10, readonly=True)
 ib.reqMarketDataType(4)  # Use free, delayed, frozen data
-contract = Forex('EURUSD')
-bars = ib.reqHistoricalData(
-  contract, endDateTime='', durationStr='30 D',
-  barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
-
-# convert to pandas dataframe (pandas needs to be installed):
-df = util.df(bars)
-print(df)
-
 
 #%%
-cerebro = bt.Cerebro()
+contract = Stock('TSLA', 'SMART', 'USD')
+available_types_of_data = ['TRADES', 'MIDPOINT', 'BID', 'ASK', 'BID_ASK', 'HISTORICAL_VOLATILITY', 'OPTION_IMPLIED_VOLATILITY']
+types_of_data = ['TRADES', 'BID_ASK', 'HISTORICAL_VOLATILITY', 'OPTION_IMPLIED_VOLATILITY']
+rth=True
+data = {}
+for typ in types_of_data:
+  data[typ] = ib.reqHistoricalData(contract, endDateTime='', durationStr='10 D',
+  barSizeSetting='1 day', whatToShow=typ, useRTH=rth)
+  print(data[typ])
+#%%
+dfs = []
+for typ in types_of_data:
+  df = util.df(data[typ])
+  df['name'] = typ
+  dfs.append(df)
+
+all_df = pd.concat(dfs)
+
+#%%
+# convert to pandas dataframe (pandas needs to be installed):
+df = util.df(bars)
+df['date'] = pd.to_datetime(df['date'])
+
+#%%
+cerebro = bt.Cerebro(preload=True)
 data = bt.feeds.PandasData(dataname=df, datetime='date')
 cerebro.adddata(data)
 
 # Add the printer as a strategy
 cerebro.addstrategy(EmaStrategy)
+# cerebro.addstrategy(MyStrategy)
 # cerebro.addstrategy(bt.Strategy)
 
 cerebro.run()
