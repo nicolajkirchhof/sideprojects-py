75fbe57fd96a6d7e47ed7d0ac01f0e90eaf8d945
Nicolaj Kirchhof <nico@docoyo.com>
2025-01-21 14:30:39 +0100

    Add script to process ETF data and visualize candlestick charts
    Introduced `docoyo_portfolio.py` to fetch, store, and display ETF data using yfinance and finplot. The script reads ETF symbols, downloads their historical data, and saves it locally for reuse. Additionally, updated `.idea/csv-editor.xml` to configure CSV attributes for ETF symbol files.

--- finance/docoyo_portfolio.py
+++ finance/docoyo_portfolio.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import os
 
 import yfinance as yf
@@ -12,13 +12,15 @@ import matplotlib.ticker as mticker
 import finplot as fplt
 import mplfinance as mpf
 
+from finance.yf_data_aquisition import df_etfs
+
 mpl.use('TkAgg')
 mpl.use('QtAgg')
 
-#%% ETFS
+# %% ETFS
 df_etfs = pd.read_csv('finance/etf_symbols.csv')
 
-#%%
+# %%
 etf_data = {}
 for etf_symbol in df_etfs['symbol']:
   filename = f'finance/y_finance_data/{etf_symbol}.csv'
@@ -28,23 +30,24 @@ for etf_symbol in df_etfs['symbol']:
   etf_data[etf_symbol] = yf.Ticker(etf_symbol).history(start="2015-01-01", interval="1d")
   pd.DataFrame(etf_data[etf_symbol]).to_csv(filename)
 
-
-#%%
+# %%
 
 portfolios = [k for k in df_etfs.keys() if 'p_' in k]
+df_portfolios = {}
 for p in portfolios:
+  df_portfolios[p] = None
+  for index, row in df_etfs.iterrows():
+    print(row['symbol'], p)
+    df = row[p] * etf_data[row['symbol']][['Open', 'High', 'Low', 'Close']] / etf_data[row['symbol']]['Open'][0]
+    if df_portfolios[p] is not None:
+        df_portfolios[p] = df_portfolios[p] + df
+    else:
+      df_portfolios[p] = df
 
-  df = etf_data[p]
-
-
-
-#%%
+# %%
 
 fplt.candlestick_ochl(df[['Open', 'Close', 'High', 'Low']])
 fplt.show()
 
-#%%
+# %%
 fig, ax = mpf.plot(df, returnfig=True, volume=True, type='candle', tight_layout=True)
-
-
-
