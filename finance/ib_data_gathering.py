1c88b12062480a7372eed13aec5683b411180fd7
Nicolaj Kirchhof <nico@docoyo.com>
2025-03-05 21:58:10 +0100

    Support continuous futures data handling in IB integration.
    Added continuous futures contracts and adapted data fetching to handle them. Updated database mapping and field name logic to support the new contract types. Improved query and write operations with proper database lookup and field formatting for continuous futures contracts.

--- finance/ib_data_gathering.py
+++ finance/ib_data_gathering.py
@@ -32,12 +32,13 @@ tws_real_port = 7497
 tws_paper_port = 7497
 api_real_port = 4002
 api_paper_port = 4002 
-ib_con.connect('127.0.0.1', api_paper_port, clientId=10, readonly=True)
-# ib_con.connect('127.0.0.1', tws_paper_port, clientId=10, readonly=True)
+# ib_con.connect('127.0.0.1', api_paper_port, clientId=10, readonly=True)
+ib_con.connect('127.0.0.1', tws_paper_port, clientId=10, readonly=True)
 ib_con.reqMarketDataType(4)  # Use free, delayed, frozen data
-
+#%%
 index_client = idb.InfluxDBClient(database='index')
 index_client.create_database('index')
+index_client.create_database('future')
 index_client_df = idb.DataFrameClient(database='index')
 # index_client.create_database('')
 # influx schema
@@ -54,9 +55,18 @@ gb_index = ib.CFD('IBGB100', 'SMART', 'GBP')
 jp_index = ib.Index('N225', 'OSE.JPN', 'JPY')
 aus_index = ib.CFD('IBAU200', 'SMART', 'AUD')
 hk_index = ib.Index('HSI', 'HKFE', 'HKD')
+#%%
+eu_futures = [ib.ContFuture(symbol=x, multiplier='1', exchange='EUREX',currency='EUR') for x in ['DAX', 'ESTX50']]
+us_futures =[*[ib.ContFuture(symbol=x[0], multiplier=x[1], exchange='CME',currency='USD') for x in [('MES', '5'), ('MNQ', '2'), ('RTY', '50')]],
+             ib.ContFuture(symbol='MYM', multiplier='0.5', exchange='CBOT',currency='USD'),
+            ib.ContFuture(symbol='VXM', multiplier='100', exchange='CFE',currency='USD')]
+jp_futures = [ib.ContFuture(symbol='N225M', multiplier='100', exchange='OSE.JPN',currency='JPY')]
+swe_futures = [ib.ContFuture(symbol='OMXS30', multiplier='100', exchange='OMS',currency='SEK')]
 
 indices = [*eu_indices, *us_indices, gb_index, es_index, jp_index, fr_index, aus_index, hk_index  ]
-contracts = indices
+# contracts = indices
+futures = [*eu_futures, *us_futures, *jp_futures, *swe_futures]
+contracts = [*futures, *indices]
 
 for contract in contracts:
   details = ib_con.reqContractDetails(contract)
@@ -65,12 +75,21 @@ for contract in contracts:
 
 #%%
 available_types_of_data = ['TRADES', 'MIDPOINT', 'BID', 'ASK', 'BID_ASK', 'HISTORICAL_VOLATILITY', 'OPTION_IMPLIED_VOLATILITY']
-types_of_data = {'IND': ['TRADES'], 'CFD': ['MIDPOINT'] }
+types_of_data = {'IND': ['TRADES'], 'CFD': ['MIDPOINT'], 'CONTFUT': ['TRADES'] }
+database_lookup = { 'IND': 'index', 'CFD': 'index', 'CONTFUT': 'future'}
 rth=False
 duration='1 W'
 barSize='1 min'
+
 startDateTime=dateutil.parser.parse('2013-06-01')
+# endDateTime=dateutil.parser.parse('2013-06-08')
 endDateTime=datetime.datetime.now()
+
+def contract_to_fieldname(contract):
+  if contract.secType == 'IND' or contract.secType == 'CFD':
+    return contract.symbol
+  if contract.secType == 'CONTFUT':
+    return f'F{contract.symbol}'
 
 #%%
 field_name_lu = {
@@ -79,28 +98,25 @@ field_name_lu = {
 }
 
 # for stock_name in stock_names:
-# contract = ib.Index('DAX', 'EUREX', 'EUR')
-# contract = contracts[3]
-# contract = ib.Contract(symbol='DAX', secType='FUT', exchange='EUREX', currency='EUR', lastTradeDateOrContractMonth='202101')
 # details = ib_con.reqContractDetails(contract)
 for contract in contracts:
   dfs_contract = {}
   for typ in types_of_data[contract.secType]:
-    c_last = index_client_df.query(f'select last("c") from {contract.symbol}')
+    c_last = index_client_df.query(f'select last("c") from {contract_to_fieldname(contract)}', database=database_lookup[contract.secType])
     if c_last:
-      current_date = pd.Timestamp(c_last[contract.symbol].index.values[0]).to_pydatetime()
+      current_date = pd.Timestamp(c_last[contract_to_fieldname(contract)].index.values[0]).to_pydatetime()
     else:
       current_date = startDateTime
     while current_date < endDateTime:
       current_date = current_date + pd.Timedelta(weeks=1)
-      endDateTimeString = current_date.strftime('%Y%m%d %H:%M:%S')
+      endDateTimeString = current_date.strftime('%Y%m%d %H:%M:%S') if not contract.secType == 'CONTFUT' else ""
       data = ib_con.reqHistoricalData(contract, endDateTime=endDateTimeString, durationStr=duration,
       barSizeSetting=barSize, whatToShow=typ, useRTH=rth)
-      print(f'{contract.symbol} => {typ} {current_date} #{len(data)}')
+      print(f'{contract_to_fieldname(contract)} => {typ} {current_date} #{len(data)}')
       if not data:
         continue
       dfs_type = ib.util.df(data).rename(columns=field_name_lu[typ]).set_index('date')
-      index_client_df.write_points(dfs_type, contract.symbol)
+      index_client_df.write_points(dfs_type, contract_to_fieldname(contract), database=database_lookup[contract.secType])
 
 #%%
 dfs = [dfsc['TRADES'] for dfsc in dfs_contract]
